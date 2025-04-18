d90ea40506d91ccdeac67ed89f5d04ca186a6d80
sakeof.capy <non-linear@ukr.net>
2025-04-18 21:45:26 +0300

    feat: cleanup

--- basket_request_generator/src/main.rs
+++ basket_request_generator/src/main.rs
@@ -13,8 +13,6 @@ async fn send_message(channel: &Channel, message: add_to_queue_request::Message)
     let payload = message.encode_to_vec();
     let queue_name = "request_queue";
 
-    // println!("Trying to send message");
-
     let confirm = channel.basic_publish(
         "",
         queue_name,
@@ -22,44 +20,8 @@ async fn send_message(channel: &Channel, message: add_to_queue_request::Message)
         &payload,
         BasicProperties::default(),
     )
+    .await?
     .await?;
-
-    // println!("Message is sent. Trying to confirm.");
-
-    confirm.await?;
-
-    // println!("Message confirmed.");
-
-    Ok(())
-}
-
-async fn listen_for_response(channel: &Channel) -> anyhow::Result<()> {
-    let _queue = channel.queue_declare("reply_to_queue", QueueDeclareOptions::default(), FieldTable::default()).await?;
-
-    // println!("Trying to do basic_consume");
-
-    let mut consumer = channel.basic_consume(
-        "reply_to_queue",
-        "client_consumer",
-        BasicConsumeOptions::default(),
-        FieldTable::default(),
-    )
-    .await?;
-
-    // println!("basic_consume done. Trying delivering.");
-
-    while let Some(delivery) = consumer.next().await {
-        // println!("Delivery spotted");
-        match delivery {
-            Ok(delivery) => {
-                let message = add_to_queue_request::Message::decode(&delivery.data[..])?;
-                channel.basic_ack(delivery.delivery_tag, BasicAckOptions::default()).await?;
-                println!("Received response from server #{}: {}", message.id, message.content);
-                break;
-            }
-            Err(e) => eprintln!("Error while consuming response: {}", e),
-        }
-    }
 
     Ok(())
 }
@@ -67,27 +29,16 @@ async fn listen_for_response(channel: &Channel) -> anyhow::Result<()> {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let connection = Connection::connect("amqp://admin:admin@rabbit:5672/%2f", lapin::ConnectionProperties::default()).await?;
-    // let connection = Connection::connect("amqp://localhost", lapin::ConnectionProperties::default()).await?;
 
-    // println!("Trying to create a channel");
     let channel = connection.create_channel().await?;
-    // println!("Channel created");
 
     let message = add_to_queue_request::Message {
         content: "Hello, server!".to_string(),
         id: next_message_id(),
     };
 
-    // loop {
-    //     println!("Dodododo");
-    //     std::thread::sleep(std::time::Duration::from_secs(3));
-    // }
-// println!("Entering the loop");
     loop {
         send_message(&channel, message.clone()).await?;
-        listen_for_response(&channel).await?;
         std::thread::sleep(std::time::Duration::from_secs(2));
     }
-
-    // Ok(())
 }
--- basket_service/src/main.rs
+++ basket_service/src/main.rs
@@ -5,27 +5,7 @@ use futures::stream::StreamExt;
 
 async fn handle_message(channel: &Channel, delivery: Delivery) -> anyhow::Result<()> {
     let message = add_to_queue_request::Message::decode(&delivery.data[..])?;
-
     println!("Received message #{}: {}", message.id, message.content);
-
-    let response = add_to_queue_request::Message {
-        content: format!("Echo: {}", message.content),
-        id: message.id,
-    };
-
-    let payload = response.encode_to_vec();
-
-    channel.basic_publish(
-        "",
-        "reply_to_queue",
-        BasicPublishOptions::default(),
-        &payload,
-        BasicProperties::default(),
-    )
-    .await?
-    .await?;
-
-    channel.basic_ack(delivery.delivery_tag, BasicAckOptions::default()).await?;
 
     Ok(())
 }
@@ -33,22 +13,10 @@ async fn handle_message(channel: &Channel, delivery: Delivery) -> anyhow::Result
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let connection = Connection::connect("amqp://admin:admin@rabbit:5672/%2f", lapin::ConnectionProperties::default()).await?;
-    // let connection = Connection::connect("amqp://localhost", lapin::ConnectionProperties::default()).await?;
-    // println!("Trying to create a channel");
     let channel = connection.create_channel().await?;
-    // println!("Channel created");
 
     let queue_name = "request_queue";
-    // println!("Trying to queue_declare");
     let _queue = channel.queue_declare(queue_name, QueueDeclareOptions::default(), FieldTable::default()).await?;
-    // println!("queue_declare done. Trying to declare reply_to_queue");
-    let _reply_to_queue = channel.queue_declare("reply_to_queue", QueueDeclareOptions::default(), FieldTable::default()).await?;
-    // println!("reply_to_queue declared.");
-
-    // loop {
-    //     println!("Dodododo");
-    //     std::thread::sleep(std::time::Duration::from_secs(3));
-    // }
 
     let mut consumer = channel.basic_consume(
         queue_name,
@@ -60,7 +28,6 @@ async fn main() -> anyhow::Result<()> {
     println!("Server is running, waiting for messages...");
 
     while let Some(delivery) = consumer.next().await {
-        // println!("Here's the delivery!");
         match delivery {
             Ok(delivery) => {
                 if let Err(e) = handle_message(&channel, delivery).await {
