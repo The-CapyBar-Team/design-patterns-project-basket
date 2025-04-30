c4ea478464e14b71d200c34148f67a1c39730297
sakeof.capy <non-linear@ukr.net>
2025-05-01 00:54:33 +0300

    fix: sending ef_message from baskets

--- basket_service/src/timeout_cleaner.rs
+++ basket_service/src/timeout_cleaner.rs
@@ -21,7 +21,7 @@ pub(crate) async fn timeout_cleaner(basket: Arc<Mutex<Basket>>) {
         let expired_holders = basket.get_expired_holders(current_timestamp);
         drop(basket);
 
-        if Err(err) = eh_sender.send_message(expired_holders) {
+        if let Err(err) = eh_sender.send_message(expired_holders).await {
             println!("ERROR SENDING: {}", err);
         }
     }
